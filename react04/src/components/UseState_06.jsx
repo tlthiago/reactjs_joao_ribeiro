b849fd7cca4d13613bf2d711e3fe26b8c14d6f10
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-12-17 11:55:53 -0300

    06_hooks_Correcao

--- react04/src/components/UseState_06.jsx
+++ react04/src/components/UseState_06.jsx
@@ -4,30 +4,31 @@ import UseState_06a from "./UseState_06a.jsx";
 
 export default function HookState06() {
 
-  const [valor, setValor] = useState(0);
+  const [valor1, setValor1] = useState(0);
+  const [valor2, setValor2] = useState(0);
 
-  function diminuir(quantidade) {
-    setValor(valor - quantidade);
+  function diminuir(valor, quantidade) {
+    valor == 1 ? setValor1(valor1 - quantidade) : setValor2(valor2 - quantidade);
   }
 
-  function aumentar(quantidade) {
-    setValor(valor + quantidade);
+  function aumentar(valor, quantidade) {
+    valor == 1 ? setValor1(valor1 + quantidade) : setValor2(valor2 + quantidade);
   }
 
   return (
     <>
       <h4>useState - Exercício 4</h4>
       
-      <p>Valor: {valor}</p>
+      <p>Valor: Adição de {valor1} + {valor2} = {valor1 + valor2}</p>
       <hr />
       <UseState_06a 
-        diminuir={() => diminuir(5)} 
-        aumentar={() => aumentar(5)} 
+        diminuir={() => diminuir(1, 5)} 
+        aumentar={() => aumentar(1, 5)} 
         valor={5}
       />
       <UseState_06a 
-        diminuir={() => diminuir(10)} 
-        aumentar={() => aumentar(10)}
+        diminuir={() => diminuir(2, 10)} 
+        aumentar={() => aumentar(2, 10)}
         valor={10} 
       />
     </>
